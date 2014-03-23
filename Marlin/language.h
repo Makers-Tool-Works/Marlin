8d109adf1a3416a1d2d36c108bebc7b83e8c5810
RoyOnWheels <royavery@hotmail.com>
2014-03-22 18:33:03 -0700

    Updated machine name/url
    Added
    #elif MOTHERBOARD == 301
    	#define MACHINE_NAME "Fusematic"
    	#define FIRMWARE_URL "http://makerstoolworks.com/"

--- Marlin/language.h
+++ Marlin/language.h
@@ -32,6 +32,9 @@
 #elif MOTHERBOARD == 77
 	#define MACHINE_NAME "3Drag"
 	#define FIRMWARE_URL "http://3dprint.elettronicain.it/"
+#elif MOTHERBOARD == 301
+	#define MACHINE_NAME "Fusematic"
+	#define FIRMWARE_URL "http://makerstoolworks.com/"
 #else
 	#ifdef CUSTOM_MENDEL_NAME
 		#define MACHINE_NAME CUSTOM_MENDEL_NAME
